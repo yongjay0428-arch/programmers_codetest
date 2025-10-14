b3cd079489747269c5bf4b9985aef55f755887b7
yongjay <yongjay0428@gmail.com>
2025-10-14 17:59:13 +0900

    파일명 수정 및 풀이 실패

--- 02.py
+++ 02.py
@@ -1,24 +1,28 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/340198
-
 def solution(mats, park):
-    answer = []
-    answer1=[]
-    answer2=[]
+    answer =0
+    answer_list = []
+    spaces=[]
+    spaces_list=[]
     y=0
     for p in park:
-        for i in range(0,len(park)):
-            if p[i] == "-1":
-                for m in mats:
-                    if set(p[i:i+m]) == {'-1'}:
-                        answer.append([y,i,i+m])
+        x = 0
+        for space in p:
+            if space == '-1':
+                spaces.append([x,y])
+            x +=1
         y+=1
-    for a in range(0,len(answer)):
+    for loc in spaces:
         for m in mats:
-            for idx in range(0, m):
-                answer1=answer.copy()
-                answer1[a][0]=answer1[a][0]+idx
-                answer2.append(answer1[a])
-    return answer2
+            if [loc[0]+m,loc[1]+m] in spaces:
+                spaces_list.append([loc,m])
+    for loc2 in spaces_list:
+        print(loc2)
+        # print(loc2) #[2, 0], 5]
+        for dist in range(1,loc2[1]):
+            if [loc2[0][0]+dist,loc2[0][1]] in spaces and [loc2[0][0],loc2[0][1]+dist] in spaces and [loc2[0][0]+dist,loc2[0][1]+dist] in spaces:
+                answer_list.append(loc2[1])
+    answer = max(answer_list)
+    return answer
 
 print(solution([5,3,2],
                [["A", "A", "-1", "B", "B", "B", "B", "-1"],
