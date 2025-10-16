2a3e0c1e5ca684685ea5ebc19b9e232eae35c016
yongjay <yongjay@gmail.com>
2025-10-16 23:18:13 +0900

    공원 풀이 완료!

--- 02.py
+++ 02.py
@@ -4,28 +4,27 @@ def solution(mats,park):
     answer = []
     a = len(park)
     b = len(park[0])
-
     for i in range(a):
         for j in range(b):
             if park[i][j] != '-1':
                 park[i][j] = '1'
-
     for m in mats:
         for i in range(a):
             for j in range(b):
                 temp_answer=[]
                 if park[i][j] =='-1':
                     temp_answer.append(square_cords(i,j,m))
-                    print(temp_answer)
+                    temp_answer2=[]
                     for x in temp_answer[0]:
-                        print(x)
-                        if a < x[0] or b < x[1]-1:
-                            break
-                        else:
-                            if park[x[0]][x[1]] != '-1':
-                                break
-                            else:
-                                answer.append(len(x))
+                        if a > int(x[0]) and b > int(x[1]) and park[x[0]][x[1]] == '-1':
+                            temp_answer2.append([x[0],x[1]])
+                        if temp_answer[0] == temp_answer2:
+                            answer.append(len(temp_answer[0]))
+    if len(answer) == 0:
+        answer = -1
+    else:
+        answer = max(answer)**0.5
+    return answer
 
 
 
