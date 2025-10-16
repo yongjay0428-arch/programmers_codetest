a47da36edf8b7de6539bccb4974691e2363a7e0c
yongjay <yongjay0428@gmail.com>
2025-10-16 17:34:34 +0900

    2번 진행 중 3번 실패

--- 02.py
+++ 02.py
@@ -1,52 +1,22 @@
-import itertools as iter
 import numpy as np
-def solution(mats, park):
-    answer =0
-    answer_list = []
-    spaces=[]
-    spaces_list=[]
-    y=0
-    for p in park:
-        x = 0
-        for space in p:
-            if space == '-1':
-                spaces.append([x,y])
-            x +=1
-        y+=1
-    for loc in spaces:
-        for m in mats:
-            if [loc[0]+m,loc[1]+m] in spaces:
-                spaces_list.append([loc,m])
-    for loc2 in spaces_list:
-        print(loc2)
-        # print(loc2) #[2, 0], 5]
-        for dist in range(1,loc2[1]):
-            if [loc2[0][0]+dist,loc2[0][1]] in spaces and [loc2[0][0],loc2[0][1]+dist] in spaces and [loc2[0][0]+dist,loc2[0][1]+dist] in spaces:
-                answer_list.append(loc2[1])
-    answer = max(answer_list)
-    return answer
-
-def solution2(mats, park):
-    answer =0
-    loc = 0
+def solution(mats,park):
+    answer = []
     a = len(park)
     b = len(park[0])
-    space = []
-    for x in range(a):
-        for y in range(b):
-            if park[x][y] != '-1':
-                park[x][y] = '1'
 
-    for x in park:
-        space.append(i for i, n in enumerate(x) if n == '-1' )
-    print(space)
-    for m in mats:
-        mlist = []
-        for i in range(m):
-            mlist.append(i)
-    print(mlist)
+    def square_coords(i, j, m):
+        return [(r, c) for r in range(i, i + m) for c in range(j, j + m)]
 
-print(solution2([5,3,2],
+    for i in range(a):
+        for j in range(b):
+            if park[i][j] != '-1':
+                park[i][j] = '1'
+    for j in range(b):
+        temp_answer=[]
+        if park[i][j] ==['1']:
+            pass
+
+print(solution([5,3,2],
                [["A", "A", "-1", "B", "B", "B", "B", "-1"],
                 ["A", "A", "-1", "B", "B", "B", "B", "-1"],
                 ["-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"],
--- 03.py
+++ 03.py
@@ -11,32 +11,15 @@ def solution(record):
         if a[0] == 'Enter':
             members[a[1]] =  a[2]
         if a[0] == 'Change':
-            changes[a[1]] =  a[2]
+            members[a[1]] = a[2]
         if a[0] == 'Leave':
             leaves.append(a[1])
 
     for x in list1:
         if x[0] == 'Enter':
-            if x[1] in changes:
-                if x[1] not in leaves:
-                    answer.append(f'{changes[x[1]]}님이 들어왔습니다.')
-                else:
-                    answer.append(f'{members[x[1]]}님이 들어왔습니다.')
-                    print(2)
-            elif x[1] in leaves:
-                answer.append(f'{members[x[1]]}님이 들어왔습니다.')
-
-            else:
-                answer.append(f'{x[2]}님이 들어왔습니다.')
-                print(4)
-        elif x[0] == 'Leave':
-            if x[1] in changes:
-                answer.append(f'{changes[x[1]]}님이 나갔습니다.')
-                print(5)
-            else:
-                answer.append(f'{members[x[1]]}님이 나갔습니다.')
+            answer.append(f'{members[x[1]]}님이 들어왔습니다.')
+        elif x[0] == 'Leave': answer.append(f'{members[x[1]]}님이 나갔습니다.')
     return answer
-
 
 print(solution(["Enter uid1234 Muzi",
                 "Enter uid4567 Prodo",
